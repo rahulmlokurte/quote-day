cc119c1b8aa327f0246540d18c2fbcd3f0abca7b
Rahul Lokurte <rahullokurte@Rahuls-MacBook-Air.local>
2020-06-07 23:15:00 +0530

    Added the homepage with basic button

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
   <div id="app">
-    <h1>Hello World</h1>
+    <HomePage />
   </div>
 </template>
 
 <script>
 
+import HomePage from './views/HomePage.vue';
+
 export default {
   name: 'App',
+  components: {
+    HomePage,
+  },
 };
 </script>
 
 <style lang="scss">
-
+#app {
+text-align: center;
+  color: deeppink;
+  margin-top: 60px;
+  font-family: Avenir,Helvetica,Arial,sans-serif;
+  -webkit-font-smoothing: antialiased;
+}
 </style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,13 +1,46 @@
 <template>
-    
+  <div>
+    <img src="./../assets/Quote.jpg" alt="quote" class="logo">
+    <h1>Quote For The Day</h1>
+    <button type="button" @click="quote">Click me for a Quote</button>
+    <p>{{clickvalue}}</p>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "HomePage"
-    }
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      clickvalue: '',
+    };
+  },
+  methods: {
+    quote() {
+      this.clickvalue = 'I am clicked';
+    },
+  },
+};
 </script>
 
 <style scoped>
+  .logo {
+    width: 10%;
+  }
 
+  img {
+    vertical-align: middle;
+  }
+
+  button {
+    background-color: deeppink;
+    text-align: center;
+    font-family: Avenir;
+    font-size: larger;
+    padding: 10px;
+    border-radius: 30px;
+    border-width: thick;
+    border-color: pink;
+    outline: none;
+  }
 </style>
