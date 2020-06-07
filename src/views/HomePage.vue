0d683a7b4851c156319c08d40296fb2901bd233a
Rahul Lokurte <rahullokurte@Rahuls-MacBook-Air.local>
2020-06-08 00:07:15 +0530

    button rotate

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -38,14 +38,34 @@ export default {
     font-family: cursive;
     font-size: 40px;
     padding: 30px;
-    border-radius: 30px;
+    border-radius: 50%;
     border-width: thin;
     border-color: deeppink;
     outline: none;
     color: deeppink;
+    cursor: pointer;
+    box-shadow: 9px 9px deeppink;
+    -webkit-animation: rotation infinite 5s;
+  }
+
+  @-webkit-keyframes rotation {
+      from {
+-webkit-transform: rotate(0deg);
+      }
+    to {
+-webkit-transform: rotate(359deg);
+    }
+  }
+  button:hover {
+    background-color: yellow;
+  }
+  button:active {
+box-shadow: 0 5px darkgreen;
+    transform: translateY(4px);
   }
 
   h1 {
-  color: aquamarine
+  color: deeppink;
+    padding-bottom: 30px;
   }
 </style>
